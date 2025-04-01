3d599f14a30a0d597b2bebc63bb8d164a0554871
Humberto Lima <humbertolima108@gmail.com>
2025-04-01 00:43:21 -0300

    [Fix] Change code to others components

---
+++ src/components/MainHeader.tsx
@@ -0,0 +1,10 @@
+export const MainHeader = () => {
+  return (
+    <>
+      <h1>
+        Tarefas Diárias <img src="/image.png" alt="Logo" height={60} />{" "}
+      </h1>
+      <h2>Adicione uma tarefa</h2>
+    </>
+  );
+};
